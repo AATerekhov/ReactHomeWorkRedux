d43d4715dd27a4006d244377f480a46d64980ec6
AATerekhov <A.A.Terekhov@mail.ru>
2025-01-19 21:13:50 +0300

    Starting routed

---
+++ my-app-redux/src/components/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+      <div className="home">
+        <h1>Welcome home!</h1>
+      </div>
+    );
+  };
+  
+  export default HomePage;
---
+++ my-app-redux/src/components/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+      <div className="home">
+        <h1>Login!</h1>
+      </div>
+    );
+  };
+  
+  export default Login;
